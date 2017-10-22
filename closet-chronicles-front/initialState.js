87fc7729186137cb1d6b2481a5caecedf8a6cf13
Sasha Bajzek <dabridginator@gmail.com>
2017-10-22 12:48:44 -0700

    First commit

---
+++ closet-chronicles-front/initialState.js
@@ -0,0 +1,53 @@
+const getInitialState = () => ({
+	outfits: {
+		occasionFilter: '',
+		isFetching: false,
+		items: []
+	},
+	tops: {
+		colorFilter: '',
+		occasionFilter: '',
+		patternFilter: '',
+		sleeveLengthFilter: '',
+		statusFilter: '',
+		typeFilter: '',
+		isFetching: false,
+		items: []
+	},
+	bottoms: {
+		colorFilter: '',
+		occasionFilter: '',
+		patternFilter: '',
+		statusFilter: '',
+		typeFilter: '',
+		isFetching: false,
+		items: []
+	},
+	shoes: {
+		colorFilter: '',
+		occasionFilter: '',
+		patternFilter: '',
+		sleeveLengthFilter: '',
+		statusFilter: '',
+		typeFilter: '',
+		isFetching: false,
+		items: []
+	},
+	coats: {
+		colorFilter: '',
+		occasionFilter: '',
+		statusFilter: '',
+		typeFilter: '',
+		isFetching: false,
+		items: []
+	},
+	accessories: {
+		colorFilter: '',
+		occasionFilter: '',
+		statusFilter: '',
+		typeFilter: '',
+		isFetching: false,
+		items: []
+	},
+	user: {}
+});
